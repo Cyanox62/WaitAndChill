71ac150aa7e2fe66b2e5d03c809f3741a6583410
Cyanox62 <cyanox62@gmail.com>
2020-11-21 22:25:49 -0500

    Made scp-106's spawn more reliable

--- WaitAndChill/EventHandler.cs
+++ WaitAndChill/EventHandler.cs
@@ -64,16 +64,7 @@ namespace WaitAndChill
                     break;
                 case RoomType.Hcz106:
                     Room = Map.Rooms.First(r => r.Type == type);
-                    GameObject cube = Physics.OverlapSphere(Room.Position, 20f).First(c => c.name == "Cube")?.gameObject;
-                    if (cube != null)
-                    {
-                        RoomPosition = cube.transform.position;
-                    }
-                    else
-                    {
-                        Room = Map.Rooms.First(r => r.Type == RoomType.EzGateA);
-                        RoomPosition = Room.Position;
-                    }
+                    RoomPosition = RoleType.Scp106.GetRandomSpawnPoint();
                     break;
                 case RoomType.Lcz173:
                     Room = Map.Rooms.First(r => r.Type == type);
